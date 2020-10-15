53569ef60883988193d1004820993dc80c71b1c6
Bob Jones <JonesBob@praintl.com>
2020-10-15 13:01:00 -0400

    refactor(features): Update formatting.mk
    Move/create formatted string constants/fucntions to/in here.

--- src/features/formatting.mk
+++ src/features/formatting.mk
@@ -11,7 +11,7 @@
 # 1. Robert (Bob) L. Jones
 #
 # CREATED: 2020-09-25
-# REVISED: 2020-09-30
+# REVISED: 2020-10-15
 # =========================================================================== #
 
 
@@ -42,6 +42,15 @@ FG_GREEN := $(ESC)[0;32m
 FG_RED := $(ESC)[0;31m
 FG_YELLOW := $(ESC)[1;33m
 
+# -- Formatted Strings -- #
+
+# Color-formatted outcome statuses, each of which is based on the return code
+# ($$?) of having run a shell command.
+DONE := $(FG_GREEN)done$(RESET).
+FAILED := $(FG_RED)failed$(RESET).
+IGNORE := $(FG_YELLOW)ignore$(RESET).
+PASSED := $(FG_GREEN)passed$(RESET).
+
 
 # =========================================================================== #
 # INTERNAL VARIABLES
@@ -58,3 +67,36 @@ subdir_var = $(FG_CYAN)$(shell basename $(@D))$(RESET)
 # Color-formatted names of makefile automatic variables.
 prereq_var = $(FG_CYAN)$<$(RESET)
 target_var = $(FG_CYAN)$@$(RESET)
+
+
+# =========================================================================== #
+# USER-DEFINED FUNCTIONS
+# =========================================================================== #
+
+
+# -- Error Capture -- #
+
+# $(call rc-str,msg,success-str)
+# Prints a message, followed by a success string ($DONE or $PASSED) or failure
+# string ($FAILED), depending on the return code ($$?) from the previously
+# executed command.
+define rc-str
+	[ $$? -eq 0 ] && echo "$1...$2" || echo "$1...$(FAILED)"
+endef
+
+# $(call status-str,msg)
+# Prints a message, followed by a success string ($DONE) or failure string
+# ($FAILED), depending on the return code ($$?) from the previously executed
+# command.  Intended for communicating the outcome status of a non-test target
+# rule.
+define status-str
+	$(call rc-str,$1,$(DONE))
+endef
+
+# $(call status-str,msg)
+# Prints a message, followed by a success string ($PASS) or failure string
+# ($FAILED), depending on the return code ($$?) from the previously executed
+# command.  Intended for communicating the outcome status of a test target rule.
+define test-str
+	$(call rc-str,$1,$(PASS))
+endef
