81491d3275b3843159e59ceb6e969ea018350eb8
Bob Jones <JonesBob@praintl.com>
2020-10-15 13:01:56 -0400

    refactor(features): Update debugging.mk
    Move/create formatted string constants/fucntions from/out of here.

--- src/features/debugging.mk
+++ src/features/debugging.mk
@@ -11,7 +11,7 @@
 # 1. Robert (Bob) L. Jones
 #
 # CREATED: 2020-09-25
-# REVISED: 2020-10-08
+# REVISED: 2020-10-15
 # =========================================================================== #
 
 
@@ -20,7 +20,7 @@
 # =========================================================================== #
 
 
-# -- Debugging & Error Capture -- #
+# -- Debugging -- #
 
 # 0 = false, 1 = true
 VERBOSE ?= 0
@@ -31,55 +31,11 @@ VERBOSE ?= 0
 # =========================================================================== #
 
 
-# -- Debugging & Error Capture -- #
+# -- Debugging -- #
 
 # A list of makefile variables to show when testing/debugging.
 VARIABLES_TO_SHOW := MAKE_DIR MAKEFILE MAKEFILE_DIR MAKEFILE_LIST
 VARIABLES_TO_SHOW += PWD VERBOSE
-
-# -- Result Strings -- #
-
-# Color-formatted outcome statuses, each of which is based on the return code
-# ($$?) of having run a shell command.
-DONE := $(FG_GREEN)done$(RESET).
-FAILED := $(FG_RED)failed$(RESET).
-IGNORE := $(FG_YELLOW)ignore$(RESET).
-PASSED := $(FG_GREEN)passed$(RESET).
-
-
-# =========================================================================== #
-# INTERNAL VARIABLES
-# =========================================================================== #
-
-
-# -- Debugging & Error Capture -- #
-
-status_result = $(call result,$(DONE)\n)
-test_result = $(call result,$(PASSED)\n)
-
-
-# =========================================================================== #
-# USER-DEFINED FUNCTIONS
-# =========================================================================== #
-
-
-# -- Debugging & Error Capture -- #
-
-# $(call result,success-string)
-# Prints a success string ($DONE or $PASSED) if the most recent return code
-# ($$?) value equals 0; otherwise, prints a failure string ($FAILED) and the
-# associated error message.
-define result
-	([ $$? -eq 0 ] && printf "$1") || \
-	(printf "$(FAILED)\n" && cat $(LOG) && echo)
-endef
-
-# $(call step,msg,success-string)
-# Prints a success string ($DONE or $PASSED) if the most recent return code
-# ($$?) value equals 0; otherwise, prints a failure string ($FAILED).
-define step
-	[ $$? -eq 0 ] && echo "$1...$2" || echo "$1...$(FAILED)"
-endef
 
 
 # =========================================================================== #
